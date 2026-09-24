d177db79c6843f04b35954a1cbfe0c3bf747c8eb
LambVasi <p8g65ty92w@privaterelay.appleid.com>
2026-09-24 15:13:59 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Baseline Tour service worker. Version 20260924104417
-const CACHE = 'baseline-tour-20260924104417';
+// Baseline Tour service worker. Version 20260924151016
+const CACHE = 'baseline-tour-20260924151016';
 const FILES = ['./', './index.html', './manifest.json', './icon-180.png', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
